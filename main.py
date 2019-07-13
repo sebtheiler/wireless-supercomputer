4dbd38aabeb9442abc3d5941d6f8f70d6a8632cb
Sebastian Keane Theiler <sebastiankt9@gmail.com>
2019-07-13 09:12:15 -0400

    Create main.py
    Blank main.py file.

---
+++ main.py
@@ -0,0 +1,11 @@
+"""Usage: python3 server.py -m main.py -f functions.py"""
+import functions
+
+PORT = ...
+
+def generate_function(user_id):
+   ...
+   return (functions.f, {}, "")
+
+def handle_received(received, user_id):
+   pass
